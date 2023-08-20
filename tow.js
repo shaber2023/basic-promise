89608e7d6d42673bf54f30f01e020e011c242a88
shaber ahmad <shaberahmad2019s@gmail.com>
2023-08-20 16:20:43 +0600

    date and time

---
+++ tow.js
@@ -0,0 +1,13 @@
+const myDate = new Date();
+
+//longe date and time
+console.log(myDate.toString());
+
+//date 
+console.log(myDate.toLocaleDateString());
+
+//date with days
+console.log(myDate.toDateString());
+
+//date with time
+console.log(myDate.toLocaleString())
